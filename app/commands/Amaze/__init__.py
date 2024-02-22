31c84ce5096552ccefe10a3e418c148c2d5a01bf
aravind0815 <as4588@njit.edu>
2024-02-21 22:08:18 -0500

    New commands created

---
+++ app/commands/Amaze/__init__.py
@@ -0,0 +1,6 @@
+from app.commands import Command
+
+
+class amazeCommand(Command):
+    def execute(self):
+        print("Have an Amazing Day ahead!!")
---
+++ app/commands/websys/__init__.py
@@ -0,0 +1,6 @@
+from app.commands import Command
+
+
+class websysCommand(Command):
+    def execute(self):
+        print("You are attending WebSystems Development class by Prof. Keith Williams")
