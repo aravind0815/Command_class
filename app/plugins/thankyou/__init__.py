12b35cf6bf6048b0f6708ec1ea3bd74dc377ae50
aravind0815 <as4588@njit.edu>
2024-03-09 23:46:33 -0500

    adding logs to plugin commands and gitignore files

--- app/plugins/thankyou/__init__.py
+++ app/plugins/thankyou/__init__.py
@@ -1,6 +1,8 @@
+import logging
 from app.commands import Command
 
 
 class thankyouCommand(Command):
     def execute(self):
+        logging.info("Hello, World!")
         print("Thank you for running the commands!")
